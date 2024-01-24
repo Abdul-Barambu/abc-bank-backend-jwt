dc8d0d2e22713fb511cb375ab0dde4a9e07d9408
Abdul-Barambu <abdulkadirbarambu1@gmail.com>
2024-01-24 15:22:50 +0100

    first commit

---
+++ abc-bank/src/test/java/com/abdul/abcbank/AbcBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdul.abcbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
